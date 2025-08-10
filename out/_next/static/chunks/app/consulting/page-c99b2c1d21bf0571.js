7789f079fad258d2aebf4c4b001a8ea1a3f405f2
Yukti Chopra <choprayukti9498@gmail.com>
2025-08-10 02:03:23 +0200

    Initial commit - Add Next.js website with static export

---
+++ out/_next/static/chunks/app/consulting/page-c99b2c1d21bf0571.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[389,404,301,895],{9006:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=9006)}),_N_E=n.O()}]);
